01a99cb2d3ac983767214369f72a4b7f25efef63
Kim Credit <kim@kimcredit.com>
2020-10-11 13:54:27 -0400

    Updates

---
+++ precache-manifest.cf273c3d6e57f093a15be996c3caa042.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "693db0cae11776c02b6a9592970df40e",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "5309dfb563627932acce",
+    "url": "/React-Portfolio/static/css/main.c29858d1.chunk.css"
+  },
+  {
+    "revision": "db2e1085dff3fb9c4594",
+    "url": "/React-Portfolio/static/js/2.d2019f7b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Portfolio/static/js/2.d2019f7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5309dfb563627932acce",
+    "url": "/React-Portfolio/static/js/main.8492c7ae.chunk.js"
+  },
+  {
+    "revision": "bcebe2d38e8b197ca4b9",
+    "url": "/React-Portfolio/static/js/runtime-main.266d4783.js"
+  }
+]);
