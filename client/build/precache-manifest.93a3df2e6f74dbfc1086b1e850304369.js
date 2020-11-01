fd785a4a4f06d165a37827d4b494c881b6a35560
Kim Credit <kim@kimcredit.com>
2020-11-01 16:51:12 -0500

    adjustments for heroku deployment

---
+++ client/build/precache-manifest.93a3df2e6f74dbfc1086b1e850304369.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "903590225628d8b98d6789dfcf5ca543",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f82a47a144b323371e8b",
+    "url": "/static/css/main.c29858d1.chunk.css"
+  },
+  {
+    "revision": "65b670a27c162f973293",
+    "url": "/static/js/2.4c578057.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4c578057.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f82a47a144b323371e8b",
+    "url": "/static/js/main.b09747be.chunk.js"
+  },
+  {
+    "revision": "dd8147bbe7734801bdde",
+    "url": "/static/js/runtime-main.e312f208.js"
+  },
+  {
+    "revision": "391260d5f7cb650e8e417aa7c7a39c07",
+    "url": "/static/media/boredjar.391260d5.png"
+  },
+  {
+    "revision": "592d8947e720b4ecc1682aa4335b813a",
+    "url": "/static/media/project_image.592d8947.png"
+  },
+  {
+    "revision": "da2a466a7b74c07700e5626a43eb1da5",
+    "url": "/static/media/snacktime.da2a466a.png"
+  }
+]);
